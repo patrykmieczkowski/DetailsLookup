2403119a82da0cdeae0f1c9aa604ab778dc63021
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2018-03-25 22:08:07 +0200

    Lifecycle methods flow

--- app/src/main/java/com/mieczkowskidev/detailslookup/MainActivity.kt
+++ app/src/main/java/com/mieczkowskidev/detailslookup/MainActivity.kt
@@ -1,12 +1,68 @@
 package com.mieczkowskidev.detailslookup
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate()")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d(TAG, "onStart()")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        Log.d(TAG, "onRestoreInstanceState()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d(TAG, "onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d(TAG, "onPause()")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?, outPersistentState: PersistableBundle?) {
+        super.onSaveInstanceState(outState, outPersistentState)
+
+        Log.d(TAG, "onSaveInstanceState()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d(TAG, "onStop()")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Log.d(TAG, "onRestart()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d(TAG, "onDestroy()")
     }
 }
