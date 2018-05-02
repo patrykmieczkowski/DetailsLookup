4aee7cd7f373f61fcf02d1ef11e04d4bde497781
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2018-05-02 22:41:05 +0200

    Constraint example activity

---
+++ app/src/main/java/com/mieczkowskidev/detailslookup/constraint/ConstraintExample.kt
@@ -0,0 +1,12 @@
+package com.mieczkowskidev.detailslookup.constraint
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstraintExample : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_example)
+    }
+}
