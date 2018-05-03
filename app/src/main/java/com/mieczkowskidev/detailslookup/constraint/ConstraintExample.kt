b9a49ab78687a34bcd927a775e1e6b19ea81a8d3
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2018-05-03 22:56:53 +0200

    constraint text view + imports

--- app/src/main/java/com/mieczkowskidev/detailslookup/constraint/ConstraintExample.kt
+++ app/src/main/java/com/mieczkowskidev/detailslookup/constraint/ConstraintExample.kt
@@ -2,6 +2,7 @@ package com.mieczkowskidev.detailslookup.constraint
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.mieczkowskidev.detailslookup.R
 
 class ConstraintExample : AppCompatActivity() {
 
