58b272e4d409fa15d1e28f38ddd563dedd2605f3
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2018-05-06 21:38:42 +0200

    Custom Dialog Activity + extensions

---
+++ app/src/main/java/com/mieczkowskidev/detailslookup/custom_alert_dialog/CustomDialogActivity.kt
@@ -0,0 +1,13 @@
+package com.mieczkowskidev.detailslookup.custom_alert_dialog
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.mieczkowskidev.detailslookup.R
+
+class CustomDialogActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_dialog)
+    }
+}
---
+++ app/src/main/java/com/mieczkowskidev/detailslookup/custom_alert_dialog/DialogExtensions.kt
@@ -0,0 +1,21 @@
+package com.mieczkowskidev.detailslookup.custom_alert_dialog
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.Fragment
+
+/**
+ * Created by Patryk Mieczkowski on 06.05.2018
+ */
+/*
+ * Notes Dialog
+ */
+inline fun Activity.showNotesAlertDialog(func: NotesDialogHelper.() -> Unit): AlertDialog =
+        NotesDialogHelper(this).apply {
+            func()
+        }.create()
+
+inline fun Fragment.showNotesAlertDialog(func: NotesDialogHelper.() -> Unit): AlertDialog =
+        NotesDialogHelper(this.activity!!).apply {
+            func()
+        }.create()
