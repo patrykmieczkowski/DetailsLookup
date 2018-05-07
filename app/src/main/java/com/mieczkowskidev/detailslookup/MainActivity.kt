0c1cc87e2bf97e9ae53917398f3406423c5d1384
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2018-05-07 22:39:32 +0200

    custom log extensions usage

--- app/src/main/java/com/mieczkowskidev/detailslookup/MainActivity.kt
+++ app/src/main/java/com/mieczkowskidev/detailslookup/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.d(TAG, "onCreate()")
+        showDLog("onCreate")
 
         setListeners()
     }
