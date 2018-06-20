4f2ae989094585e11d1fbeac7fc05cee6120a384
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2018-06-20 23:03:36 +0200

    SW activity

---
+++ app/src/main/java/com/mieczkowskidev/detailslookup/paging/SWActivity.kt
@@ -0,0 +1,12 @@
+package com.mieczkowskidev.detailslookup.paging
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SWActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sw)
+    }
+}
