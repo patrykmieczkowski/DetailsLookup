fc94ec944b2b990db409852f4f56ddb9d8d1f092
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2018-07-09 22:29:42 +0200

    Looper, handler and thread

--- app/src/main/java/com/mieczkowskidev/detailslookup/threading/ThreadClassicController.java
+++ app/src/main/java/com/mieczkowskidev/detailslookup/threading/ThreadClassicController.java
@@ -1,12 +1,16 @@
 package com.mieczkowskidev.detailslookup.threading;
 
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
+import android.util.Log;
 
 /**
  * Created by Patryk Mieczkowski on 09.07.2018
  */
 public class ThreadClassicController {
+
+    public static final String TAG = ThreadClassicController.class.getSimpleName();
 
     private void handlerMethod() {
 
@@ -18,9 +22,23 @@ public class ThreadClassicController {
 
     class MyFirstThread extends Thread {
 
+        public Handler handler;
+
         @Override
         public void run() {
             super.run();
+            Looper.prepare();
+
+            handler = new Handler() {
+                @Override
+                public void handleMessage(Message msg) {
+                    super.handleMessage(msg);
+
+                    Log.d(TAG, "handleMessage: " + msg);
+                }
+            };
+
+            Looper.loop();
         }
     }
 
